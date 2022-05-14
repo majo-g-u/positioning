4dd2e6cd3c44e5310013055c4918501aeee83d16
Maria Gomez <maria.gomez@jbknowledge.com>
2022-05-14 16:12:14 -0300

    re order folders and files, added some variables to practice JS

---
+++ JavaScript/script.js
@@ -0,0 +1,44 @@
+//console.log("JAVASCRIPT IS LINKED")
+
+var personName = "Majo";
+var pets = 1;
+var funFact = "can't live wout SODA";
+
+console.log("My name is:" + " " + personName + ".");
+console.log("I have" + " " + pets + " " + "pet/s.");
+console.log("Fun Fact:" + " " + funFact + ".");
+
+var personName = "His name is Matt";
+var pets = 0;
+var funFact = "can't live wout lemon";
+
+console.log("His name is:" + " " + personName + ".");
+console.log("He has" + " " + pets + " " + "pet/s.");
+console.log("Fun Fact:" + " " + funFact + ".");
+
+var one = 64;
+var two = "636573";
+var three = false;
+var four = 64.55;
+var five = "Howdy";
+var six;
+
+//logs number
+console.log(typeof one);
+
+//logs string
+console.log(typeof two);
+
+//logs boolean
+console.log(typeof three);
+
+//logs number
+console.log(typeof four);
+
+//logs string
+console.log(typeof five);
+
+//logs undefined
+console.log(typeof six);
+
+//I stopped at 1.27 VIDEO 
