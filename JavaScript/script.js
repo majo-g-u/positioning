6155178492737367e8896bc3601901a143915311
Maria Gomez <maria.gomez@jbknowledge.com>
2022-05-14 22:11:10 -0300

    added more javascript, worked with OPERATORS

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -41,4 +41,72 @@ console.log(typeof five);
 //logs undefined
 console.log(typeof six);
 
-//I stopped at 1.27 VIDEO 
+//LOGICAL OPERATORS
+var a ="50";
+var b = 50;
+var c = 100;
+var d = c % b;
+var e = c / 2;
+
+var expression1 = (b === e);
+var expression2 = (e < d);
+
+//use comparison operators so all expressions log as TRUE
+
+console.log(a !== b);
+console.log(b === e);
+console.log(c > b);
+console.log(d > 0);
+
+//use logical operators so all expressions log as TRUE
+console.log(expression1 && !expression2);
+console.log(expression1 || expression2);
+
+//Conditional Operators
+var isSleepy = true;
+hungerLevel = 50;
+lunchTime = true;
+lunchBill = 11;
+
+if(isSleepy){
+ console.log(" i am getting tired")
+}
+
+if (hungerLevel > 1) {
+    console.log("Im hungry")
+} else {
+    console.log("Im OK")
+}
+
+if (!lunchTime) {
+    console.log("time to eat")
+} else {
+    console.log("eat later")
+}
+
+if (lunchBill > 10) {
+    console.log("cost 1")
+} else if (lunchBill >= 10 && lunchBill < 15){
+    console.log("cost 2")
+} else{
+console.log("cost 3")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
